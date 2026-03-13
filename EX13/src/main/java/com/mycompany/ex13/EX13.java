71600df83dfc54b17902c1cba294f55a878bf68a
Kaynã Henrique Silva Bento <119448136+kaynao@users.noreply.github.com>
2026-03-13 13:03:31 -0300

    Add files via upload

---
+++ EX13/src/main/java/com/mycompany/ex13/EX13.java
@@ -0,0 +1,15 @@
+package com.mycompany.ex13;
+
+import classes.Filme;
+import classes.ProjetoCadFilme;
+/**
+ *
+ * @author 12303802644
+ */
+public class EX13 {
+
+    public static void main(String[] args) {
+        // chama o main de ProjetoCadFilme
+        ProjetoCadFilme.main(args);
+    }
+}
